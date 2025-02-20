fb906e931b16236d1887d2ccc595826c67a32c61
Ruhan Rouf <Ruhanrouf17@gmail.com>
2025-02-20 19:21:44 +0600

    added textarea

--- src/Components/AddJoke.tsx
+++ src/Components/AddJoke.tsx
@@ -1,34 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
-import { Input, Label } from "./Form";
 import { cn } from "../lib/utils";
+import { Input, Label, Textarea } from "./Form";
 
 export function AddJoke({ handleSubmit }: any) {
-
-
-
   return (
-    <div
-      className="w-full max-w-md p-4 mx-auto bg-black rounded-none md:rounded-2xl md:p-8 shadow-input"
-    >
+    <div className="w-full max-w-md p-4 mx-auto bg-black rounded-none md:rounded-2xl md:p-8 shadow-input">
       <h2 className="text-xl font-bold text-neutral-200">Noco Jokes</h2>
       <p className="max-w-sm mt-2 text-sm text-neutral-300">
         submit your cutest Joke
       </p>
 
-      <form className="my-8" onSubmit={handleSubmit} autoComplete="off">
+      <form
+        className="my-8"
+        onSubmit={handleSubmit}
+        autoComplete="off">
         <LabelInputContainer className="mb-4">
           <Label htmlFor="name">Name</Label>
-          <Input name="name" placeholder="your name" type="text" />
+          <Input
+            name="name"
+            placeholder="your name"
+            type="text"
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="joke">Joke</Label>
-          <Input name="joke" placeholder="tell me :D" type="text" />
+          <Textarea
+            name="joke"
+            placeholder="tell me :D"
+            className="h-20"
+          />
         </LabelInputContainer>
         <button
           className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
+          type="submit">
           Submit Joke &rarr;
           <BottomGradient />
         </button>
